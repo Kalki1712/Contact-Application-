27ca476cea80f77bcc96279fb36d58078ea5524d
kalki1712 <kalkiganapathi75@gmail.com>
2026-03-25 14:49:23 +0530

    Styles Using Tailwind Css

--- frontend/src/components/atoms/AuthCard.jsx
+++ frontend/src/components/atoms/AuthCard.jsx
@@ -1,21 +1,38 @@
-import { Box, Paper, Typography } from "@mui/material";
-
 const AuthCard = ({ title, children }) => {
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
-    >
-      <Paper sx={{ p: 4, width: 350 }}>
-        <Typography variant="h5" textAlign="center">
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="bg-white shadow-md rounded-lg p-8 w-full max-w-sm">
+        <h5 className="text-xl font-semibold text-center mb-4">
           {title}
-        </Typography>
+        </h5>
         {children}
-      </Paper>
-    </Box>
+      </div>
+    </div>
   );
 };
 
 export default AuthCard;
+
+
+
+// import { Box, Paper, Typography } from "@mui/material";
+
+// const AuthCard = ({ title, children }) => {
+//   return (
+//     <Box
+//       display="flex"
+//       justifyContent="center"
+//       alignItems="center"
+//       minHeight="100vh"
+//     >
+//       <Paper sx={{ p: 4, width: 350 }}>
+//         <Typography variant="h5" textAlign="center">
+//           {title}
+//         </Typography>
+//         {children}
+//       </Paper>
+//     </Box>
+//   );
+// };
+
+// export default AuthCard;
